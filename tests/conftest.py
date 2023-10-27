18ba9934a53f06f28ee84344c7a4c4cce729aad6
lambotik <lambotik1988@gmail.com>
2023-10-27 23:41:57 +0300

    Added: simple test for check.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,12 @@ from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="function", autouse=True)
-def driver(request):
+def driver():
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=options)
-    request.cls.driver = driver
     yield driver
     driver.quit()
