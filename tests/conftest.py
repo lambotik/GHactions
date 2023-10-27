b565955bdcf966fe760ced79d6854b31a0c1f7ba
lambotik <lambotik1988@gmail.com>
2023-10-27 23:56:09 +0300

    Added: simple test for check.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,15 @@
 import pytest
 from selenium import webdriver
+
 from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope='function')
 def driver():
     options = Options()
-    # options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
+    options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(10)
+    driver.maximize_window()
     yield driver
     driver.quit()
