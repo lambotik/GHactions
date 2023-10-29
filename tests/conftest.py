d666fde14b7bff027713aee59d7c4b2ade983779
lambotik <lambotik1988@gmail.com>
2023-10-29 18:34:56 +0300

    Added:reports

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope='function')
 def driver():
     options = Options()
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
     driver.maximize_window()
